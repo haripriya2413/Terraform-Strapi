79b526e31342e22931e746ee0485b9ca5b498623
haripriya2413 <haripriyagh@gmail.com>
2024-06-20 17:22:53 +0530

    hello

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "strapi" {
   ami           = var.ami  # Ubuntu 20.04 LTS AMI
   instance_type = var.instance_type
-  key_name      = aws_key_pair.deployer.key_name  # Replace with your key pair name
+  key_name      = aws_key_pair.strapi_keypair.key_name # Replace with your key pair name
     tags = {
         Name = "Strapi-Instance"
      }
@@ -19,20 +19,42 @@ provisioner "remote-exec" {
              # "cd /srv/strapi",
               "if [ ! -d /srv/strapi ]; then sudo git clone https://github.com/haripriya2413/Strapi-CICD /srv/strapi; else cd /srv/strapi && sudo git pull origin main; fi",
               "cd /srv/strapi",
+             
+
+          # Install Strapi globally
+          "sudo npm install strapi@beta -g",
+
+          # Create a new Strapi project
+          "strapi new priya-project --dbclient=sqlite",
+
+          # Navigate to the new project directory
+          "cd priya-project",
+
+          # Start the Strapi application using pm2
+          "pm2 start npm --name 'strapi' -- run develop"
+             
+  
+
             
     ]
   }
    connection {
     type        = "ssh"
     user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
-    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
+    private_key = tls_private_key.strapi_key.private_key_pem  # Replace with the path to your private key
     host        = self.public_ip
   }
+ 
   security_groups = [aws_security_group.strapi-sg.name]
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = file("~/.ssh/id_rsa.pub")
+ 
+resource "tls_private_key" "strapi_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
+resource "aws_key_pair" "strapi_keypair" {
+  key_name   = "strapi-keypair2"
+  public_key = tls_private_key.strapi_key.public_key_openssh
+}
